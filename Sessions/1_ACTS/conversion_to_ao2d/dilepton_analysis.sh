8d3b346af93d797779272efdd014677f3c09a895
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-10 13:28:06 +0100

    U

--- Sessions/1_ACTS/conversion_to_ao2d/dilepton_analysis.sh
+++ Sessions/1_ACTS/conversion_to_ao2d/dilepton_analysis.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 rm AnalysisResults.root
-o2-analysis-alice3-tracking-performance --aod-file AO2D_converted.root -b
+# o2-analysis-alice3-tracking-performance --aod-file AO2D_converted.root -b
+
+
+# o2-analysis-onthefly-tofpid -b --configuration json://configuration.json --aod-file AO2D_converted.root
+o2-analysis-alice3-dilepton -b --configuration json://configuration.json --aod-file AO2D_converted.root
