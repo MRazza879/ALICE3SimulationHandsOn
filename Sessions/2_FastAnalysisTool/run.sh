dcc0421f62d16f83be97263ba0e5a036643d2266
Nicolò Jacazio <nicolo.jacazio@cern.ch>
2025-12-11 06:06:32 +0100

    Add ALI

--- Sessions/2_FastAnalysisTool/run.sh
+++ Sessions/2_FastAnalysisTool/run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-
 root -l -b <<EOF
+.L AliLog.cxx+
+.L AliCheb3DCalc.cxx+
+.L AliCheb3D.cxx+
+.L AliMagWrapCheb.cxx+
+.L AliMagFast.cxx+
+.L AliMagF.cxx+
+
+.L AliVMisc.cxx+
+.L AliPDG.cxx++
+.L AliVVertex.cxx+
+.L AliPID.cxx+
+.L AliVParticle.cxx+
+.L AliVTrack.cxx+
+.L AliExternalTrackParam.cxx+
+
     .L DetectorK/HistoManager.cxx+
     .L DetectorK/DetectorK.cxx+
     .L lutWrite.cc
@@ -17,6 +31,6 @@ root -l -b <<EOF
     TDatabasePDG::Instance()->AddParticle("helium3", "helium3", 2.80839160743, kTRUE, 0.0, 6, "Nucleus", 1000020030);
     TDatabasePDG::Instance()->AddAntiParticle("anti-helium3", -1000020030);
 
-    lutWrite_default("asd,dat", 11, 50, 20);
+    lutWrite_detector("asd,dat", 11, 50, 20);
 
 EOF
